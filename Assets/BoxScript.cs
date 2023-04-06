d4de0bd53a8aaca9e91575b6a41fe1741b606ac8
Andrew-Buh <buhun98@mail.ru>
2023-04-06 10:09:47 +0300

    add distance

--- Assets/BoxScript.cs
+++ Assets/BoxScript.cs
@@ -12,7 +12,7 @@ public class BoxScript : MonoBehaviour
         Ray ray = new Ray(transform.position, transform.forward*2.9f);
         Debug.DrawRay(transform.position, transform.forward*2.9f, Color.blue);
         RaycastHit hit;
-        if(Physics.Raycast(ray, out hit))
+        if(Physics.Raycast(ray, out hit,2.9f))
         {
             pointer.position=hit.point;
             hit.collider.gameObject.GetComponent<Renderer>().material.color=Color.blue;
